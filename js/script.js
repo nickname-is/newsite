9fe3fb02c0ab50f28a92b8202c75e786b20abc90
nickname-is <119123781+nickname-is@users.noreply.github.com>
2023-02-16 17:53:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* Подключение navbar во все элементы с id="nav" */
-$("#nav").load("../components/navbar.html");
-$("#menu").load("../components/navbar.html");
+$("#nav").load("components/navbar.html");
+$("#menu").load("components/navbar.html");
 
 $(document).on('click', '.menu-btn-open', () => {
     document.body.classList.toggle("menu-opened")
